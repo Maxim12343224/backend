592aa1d6bc7b40c992e1de6b6568dde795210c9d
Maxim Efremov <efremoffmak@yandex.ru>
2025-05-22 22:52:08 +0300

    для задачи sum

---
+++ sprint1/problems/sum/solution/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    std::cin >> x >> y;
+    std::cout << x + y << std::endl;
+    return 0;
+    // Выведите сумму чисел x и y в std::cout.
+}
