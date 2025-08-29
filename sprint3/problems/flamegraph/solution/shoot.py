a15e05719fc4564dca904399bdc18ecc16c4fe5b
Maxim Efremov <efremoffmak@yandex.ru>
2025-08-29 19:34:33 +0300

    Тестирование  решения

--- sprint3/problems/flamegraph/solution/shoot.py
+++ sprint3/problems/flamegraph/solution/shoot.py
@@ -11,8 +11,8 @@ SEED = 123456789
 random.seed(SEED)
 
 AMMUNITION = [
-    'localhost:8080/api/v1/maps/map1',
-    'localhost:8080/api/v1/maps'
+    'http://localhost:8080/api/v1/maps/map1',
+    'http://localhost:8080/api/v1/maps'
 ]
 
 SHOOT_COUNT = 100
@@ -25,19 +25,24 @@ def start_server():
     return parser.parse_args().server
 
 
-def run(command, output=None):
-    process = subprocess.Popen(shlex.split(command), stdout=output, stderr=subprocess.DEVNULL)
+def run(command, output=None, input_pipe=None):
+    process = subprocess.Popen(shlex.split(command), 
+                              stdout=output, 
+                              stderr=subprocess.DEVNULL,
+                              stdin=input_pipe)
     return process
 
 
 def stop(process, wait=False):
-    if process.poll() is None and wait:
-        process.wait()
-    process.terminate()
+    if process.poll() is None:
+        if wait:
+            process.wait()
+        else:
+            process.terminate()
 
 
 def shoot(ammo):
-    hit = run('curl ' + ammo, output=subprocess.DEVNULL)
+    hit = run(f"curl -s {ammo}", output=subprocess.DEVNULL)
     time.sleep(COOLDOWN)
     stop(hit, wait=True)
 
@@ -57,8 +62,7 @@ def perf_record(pid, output_file="perf.data"):
 def stop_perf(perf_process):
     """Корректно останавливает perf record"""
     perf_process.send_signal(signal.SIGINT)
-    if perf_process.poll() is None:
-        perf_process.wait()
+    perf_process.wait()
 
 
 def generate_flamegraph(perf_data="perf.data", output_svg="graph.svg"):
@@ -67,43 +71,52 @@ def generate_flamegraph(perf_data="perf.data", output_svg="graph.svg"):
     if not os.path.exists(perf_data):
         raise FileNotFoundError(f"File {perf_data} not found")
     
-   
+    # Правильный пайплайн: perf script | stackcollapse | flamegraph
     perf_script = run(f"perf script -i {perf_data}", output=subprocess.PIPE)
     
-    stackcollapse = run("./FlameGraph/stackcollapse-perf.pl", output=subprocess.PIPE)
-    stackcollapse.stdin = perf_script.stdout
+    # Ждем завершения perf script и передаем вывод в stackcollapse
+    perf_output, _ = perf_script.communicate()
     
+    stackcollapse = run("./FlameGraph/stackcollapse-perf.pl", 
+                       input_pipe=subprocess.PIPE,
+                       output=subprocess.PIPE)
+    
+    # Ждем завершения stackcollapse
+    stackcollapse_output, _ = stackcollapse.communicate(input=perf_output)
+    
+    # Записываем результат в файл
     with open(output_svg, "w") as svg_file:
-        flamegraph = run("./FlameGraph/flamegraph.pl", output=svg_file)
-        flamegraph.stdin = stackcollapse.stdout
-        flamegraph.wait()
+        flamegraph = run("./FlameGraph/flamegraph.pl", 
+                        input_pipe=subprocess.PIPE,
+                        output=svg_file)
+        flamegraph.communicate(input=stackcollapse_output)
 
 
-
+# Основной код
 server_command = start_server()
 server_process = run(server_command)
 
 try:
+    # Даем серверу время на запуск
+    time.sleep(3)
     
-    time.sleep(2)
-    
-    
+    # Запускаем perf record
     perf_process = perf_record(server_process.pid)
     
-    
+    # Даем perf время на запуск
     time.sleep(1)
     
-   
+    # Выполняем обстрел
     make_shots()
     
-   
+    # Останавливаем perf
     stop_perf(perf_process)
     
-    
+    # Генерируем флеймграф
     generate_flamegraph()
     
 finally:
-    
+    # Гарантированно останавливаем сервер
     stop(server_process, wait=True)
 
 time.sleep(1)
