798948bfe0f372970dcd7991e6c526dcc94daa86
Maxim Efremov <efremoffmak@yandex.ru>
2025-11-09 09:04:36 +0300

    Тестирование  решения

--- sprint4/problems/bookypedia-2/solution/src/ui/view.h
+++ sprint4/problems/bookypedia-2/solution/src/ui/view.h
@@ -1,4 +1,3 @@
-// solution/src/ui/view.h
 #pragma once
 #include <iosfwd>
 #include <optional>
@@ -49,7 +48,7 @@ namespace ui {
 
         std::optional<detail::AddBookParams> GetBookParams(std::istream& cmd_input) const;
         std::optional<std::string> SelectAuthor() const;
-        std::optional<std::string> SelectBook(const std::string& title = "") const;
+        std::optional<std::string> SelectBook(const std::string& title = "", const std::string& pre_choice = "") const;
         std::vector<detail::AuthorInfo> GetAuthors() const;
         std::vector<detail::BookInfo> GetBooks() const;
         std::vector<detail::BookInfo> GetAuthorBooks(const std::string& author_id) const;
